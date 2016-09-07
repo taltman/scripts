5797d6d053fef95b8ba143e3ae2f96a663ba273d
Tomer Altman <tomer.altman@wholebiome.com>
2016-09-07 11:29:39 -0700

    Init commit!

---
+++ unix_utils/split-tar-by-dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#### split-tar-by-dir.sh
+##
+## Description:
+## Given a tar ball, create a set of smaller tar balls, where each tar
+## ball is a top-level directory that was present in the given tar
+## ball.
+
+
+
